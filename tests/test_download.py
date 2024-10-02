00a2f5462caea8355e7dfb08970678e1526ef8bd
Arpita Patnaik <patnaikarpita10@gmail.com>
2024-10-02 17:56:42 -0400

    Update test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -3,7 +3,7 @@ import urllib.request
 import os 
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from project0 import fetchincidents, extractincidents, createdb, populatedb, status
+from project0.project0 import fetchincidents, extractincidents, createdb, populatedb, status
 import shutil
 import sqlite3
 
