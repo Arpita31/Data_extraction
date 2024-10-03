6d64e127be277625249432d4d5e3802da231e237
Arpita Patnaik <patnaikarpita10@gmail.com>
2024-10-02 23:26:10 -0400

    removed project0

--- project0/main.py
+++ project0/main.py
@@ -36,7 +36,7 @@ def fetchincidents(url):
                 if re.search("daily_incident_summary.pdf", link.get('href')):                
                     pdf_url = urllib.parse.urljoin(url, link.get('href'))  # Handle relative URLs
                     response = urllib.request.urlopen(pdf_url)
-                    temp = os.path.join(os.getcwd(), 'project0/resources')
+                    temp = os.path.join(os.getcwd(), 'resources')
                     file_path = os.path.join(temp, "Daily_Incident_Summary.pdf")
                     
                     if not os.path.exists(temp):
